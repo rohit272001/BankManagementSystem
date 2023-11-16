15af8e95f93bc28d780e46c09bbbfbafa2ca6230
Rohit Kumar <63872936+rohit272001@users.noreply.github.com>
2023-11-16 14:42:27 +0530

    Delete BankManagementSpring directory

--- BankManagementSpring/src/main/java/com/fis/bankmanagementspring/repository/TransRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fis.bankmanagementspring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fis.bankmanagementspring.model.Trans;
-
-public interface TransRepository extends CrudRepository<Trans, Integer> {
-
-}
--- BankManagementSpring/src/main/java/com/fis/bankmanagementspring/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fis.bankmanagementspring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fis.bankmanagementspring.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
